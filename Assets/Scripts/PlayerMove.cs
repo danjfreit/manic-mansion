d06502111ad6b82fdc9ee4c168e671e7db7e2dba
Tyler White <twigwhite@gmail.com>
2019-01-26 10:24:39 -0600

    Removed input axises(?) that is not needed. Player move, only to be used in Broken. Removed 3D things from the scene (lights, skybox)

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Transform _transform;
+
+    public float Speed;
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _transform = transform;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var dir = Input.GetAxis("Vertical") * Vector3.up * Speed * Time.deltaTime;
+        _transform.Translate(dir);
+    }
+}
