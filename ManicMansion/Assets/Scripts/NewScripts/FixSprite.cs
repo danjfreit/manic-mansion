a2c2a6b848981baa4299e668fd09e67015ae7465
Daniel Freitas <dan.j.freit@gmail.com>
2019-04-01 05:11:27 -0500

    Add  Fixable observer/observable functionality

---
+++ ManicMansion/Assets/Scripts/NewScripts/FixSprite.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Fixable))]
+public class FixSprite : MonoBehaviour, IFixObserver {
+    public Sprite fixedSprite;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start() {
+        GetComponent<Fixable>().AddObserver(this);
+    }
+
+    public void OnFix() {
+        spriteRenderer.sprite = fixedSprite;
+    }
+}
---
+++ ManicMansion/Assets/Scripts/NewScripts/Fixable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Fixable : MonoBehaviour {
+    private List<IFixObserver> observers;
+
+    private void Awake() {
+        observers = new List<IFixObserver>();
+    }
+
+    private void Update() {
+        if (Time.time > 1.5f)
+            Fix();
+    }
+
+    public void AddObserver(IFixObserver observer) {
+        observers.Add(observer);
+    }
+
+    public void RemoveObserver(IFixObserver observer) {
+        observers.Remove(observer);
+    }
+
+    public void Fix() {
+        foreach (IFixObserver observer in observers) {
+            observer.OnFix();
+        }
+    }
+}
