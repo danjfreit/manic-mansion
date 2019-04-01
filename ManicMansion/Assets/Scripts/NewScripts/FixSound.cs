60e31074b0f412dfbb654515958693c1c37c7e1d
Daniel Freitas <dan.j.freit@gmail.com>
2019-04-01 07:30:27 -0500

    Add FixSound component

---
+++ ManicMansion/Assets/Scripts/NewScripts/FixSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Tools;
+
+
+public class FixSound : FixObserver {
+    public AudioClip fixedSound;
+    public AudioClip badFixedSound;
+
+    private AudioSource audioSource;
+
+    protected new void Awake() {
+        base.Awake();
+        audioSource = FindObjectOfType<AudioSource>();
+    }
+
+    public override void OnFix(ToolEnum tool) {
+        if (tool == toolComp.tool) {
+            if (toolInv.values[toolComp.tool] > 0)
+                audioSource.PlayOneShot(fixedSound);
+            else
+                audioSource.PlayOneShot(badFixedSound);
+        }
+    }
+}
